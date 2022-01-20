f744096cb5b08b0589ff04a247cf6e2358670a50
Sinpo <Sinpo96@163.com>
2022-01-20 09:21:11 +0800

    修改测试代码

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import reactDom from 'react-dom';
 
-const App = () => (
-    <div>
-        test page
-    </div>
-);
+const App = () => {
+    const [text, setText] = useState("")
+
+    return (
+        <div>
+            <span>test {text}</span>
+            <button onClick={() => setText(String(Math.random()))}>click</button>
+        </div>
+    )
+};
 reactDom.render(<App/>, document.getElementById('root'));
